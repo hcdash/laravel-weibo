7aa60e571aad7fb0765c655e23ed7044d31f4fe9
李祥 <2661272907@qq.com>
2024-07-21 22:04:03 +0800

    测试分支提交代码

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,7 +9,7 @@ class SessionsController extends Controller
     public function create()
     {
         return view('sessions.create');
-        
+
     }
 
     public function store(Request $request)
